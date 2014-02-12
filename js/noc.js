ac0881555c6b273772cf3deda61eeafbdc140db2
kelvin <kowochen@gmail.com>
2014-02-12 13:32:56 +0800

    fix mousemove event

--- js/noc.js
+++ js/noc.js
@@ -1,37 +1,37 @@
 var WIDTH = 640;
 var HEIGHT = 360;
 function EgWidget(id, draw, update, reset) {
-    this.canvas = document.getElementById(id);    
+    this.canvas = document.getElementById(id);
     this.canvas.width = WIDTH;
     this.canvas.height = HEIGHT;
     this.width = this.canvas.width;
     this.height = this.canvas.height;
-    this.context = this.canvas.getContext('2d');    
+    this.context = this.canvas.getContext('2d');
     this.draw = draw;
     this.update = update;
     this.reset = reset;
     this.pause = false;
-    
+
     this.mouse = new Vector(0, 0);
-    this.canvas.onmousemove = this.onMousemove.bind(this);           
-    
+    this.canvas.onmousemove = this.onMousemove.bind(this);
+
     if (update) {
         var pauseBtn = document.createElement('button');
         pauseBtn.innerHTML = 'PAUSE';
         pauseBtn.onclick = this.onPause.bind(this);
         this.canvas.parentNode.insertBefore(pauseBtn, this.canvas.nextSibling);
-        
+
         var resetBtn = document.createElement('button');
         resetBtn.innerHTML = 'RESET';
         resetBtn.onclick = this.onReset.bind(this);
         this.canvas.parentNode.insertBefore(resetBtn, this.canvas.nextSibling);
-    }    
+    }
 }
-EgWidget.prototype.run = function() {    
+EgWidget.prototype.run = function() {
     if (this.pause) {
         return;
     }
-    
+
     if (this.update) {
         requestAnimationFrame(this.run.bind(this));
         this.update();
@@ -45,17 +45,27 @@ EgWidget.prototype.onPause = function() {
     }
 };
 EgWidget.prototype.onReset = function() {
-    this.reset && this.reset();     
-    this.clear();   
+    this.reset && this.reset();
+    this.clear();
     if (this.pause) {
         requestAnimationFrame(this.run.bind(this));
         this.pause = false;
-    }    
+    }
 };
 EgWidget.prototype.onMousemove = function(e) {
     if (!this.pause) {
-        this.mouse.set(e.layerX, e.layerY);
-    }    
+        var x;
+        var y;
+        if (e.pageX || e.pageY) {
+            x = e.pageX;
+            y = e.pageY;
+        } else {
+            x = e.clientX + document.body.scrollLeft + document.documentElement.scrollLeft;
+            y = e.clientY + document.body.scrollTop + document.documentElement.scrollTop;
+        }
+        this.mouse.x = x - this.canvas.offsetLeft;
+        this.mouse.y = y - this.canvas.offsetTop;
+    }
 };
 EgWidget.prototype.clear = function() {
     //this.canvas.width = this.canvas.width;
@@ -82,7 +92,7 @@ var spare = null;
 Random.gauss = function(mu, sigma) {
     mu = mu || 0;
     sigma = sigma || 1;
-    
+
     if (spare !== null) {
         var v = mu + sigma * spare;
         spare = null;
@@ -114,10 +124,10 @@ var permutation = [151,160,137,91,90,15,
     251,34,242,193,238,210,144,12,191,179,162,241,81,51,145,235,249,14,239,107,
     49,192,214,31,181,199,106,157,184,84,204,176,115,121,50,45,127,4,150,254,
     138,236,205,93,222,114,67,29,24,72,243,141,128,195,78,66,215,61,156,180];
-    
+
 var p = [];
 for (var i = 0; i < 256 ; i++) {
-    p[256+i] = p[i] = permutation[i]; 
+    p[256+i] = p[i] = permutation[i];
 }
 
 var fade = function(t) {
@@ -127,37 +137,37 @@ var lerp = function(t, a, b) {
     return a + t * (b - a);
 };
 var grad = function(hash, x, y, z) {
-    var h = hash & 15;  
-    var u = h < 8 ? x : y; 
+    var h = hash & 15;
+    var u = h < 8 ? x : y;
     var v = h < 4 ? y : h == 12 || h == 14 ? x : z;
     return ((h & 1) == 0 ? u : -u) + ((h & 2) == 0 ? v : -v);
 };
-Random.noise = function(x, y, z) {    
+Random.noise = function(x, y, z) {
     x = x || 0;
     y = y || 0;
     z = z || 0;
-      
-    var X = Math.floor(x) & 255; 
+
+    var X = Math.floor(x) & 255;
     var Y = Math.floor(y) & 255;
     var Z = Math.floor(z) & 255;
-    
-    x -= Math.floor(x);           
-    y -= Math.floor(y);   
+
+    x -= Math.floor(x);
+    y -= Math.floor(y);
     z -= Math.floor(z);
-      
-    var u = fade(x);    
-    var v = fade(y);  
+
+    var u = fade(x);
+    var v = fade(y);
     var w = fade(z);
-    
-    var A = p[X  ]+Y, AA = p[A]+Z, AB = p[A+1]+Z; 
-    var B = p[X+1]+Y, BA = p[B]+Z, BB = p[B+1]+Z; 
-    
-    return lerp(w, lerp(v, lerp(u, grad(p[AA  ], x  , y  , z   ), 
-                                   grad(p[BA  ], x-1, y  , z   )), 
-                           lerp(u, grad(p[AB  ], x  , y-1, z   ),  
+
+    var A = p[X  ]+Y, AA = p[A]+Z, AB = p[A+1]+Z;
+    var B = p[X+1]+Y, BA = p[B]+Z, BB = p[B+1]+Z;
+
+    return lerp(w, lerp(v, lerp(u, grad(p[AA  ], x  , y  , z   ),
+                                   grad(p[BA  ], x-1, y  , z   )),
+                           lerp(u, grad(p[AB  ], x  , y-1, z   ),
                                    grad(p[BB  ], x-1, y-1, z   ))),
-                   lerp(v, lerp(u, grad(p[AA+1], x  , y  , z-1 ),  
-                                   grad(p[BA+1], x-1, y  , z-1 )), 
+                   lerp(v, lerp(u, grad(p[AA+1], x  , y  , z-1 ),
+                                   grad(p[BA+1], x-1, y  , z-1 )),
                            lerp(u, grad(p[AB+1], x  , y-1, z-1 ),
                                    grad(p[BB+1], x-1, y-1, z-1 ))))
             * 0.5 + 0.5; // from 0.0 to 1.0, rather than -1.0 to 1.0
@@ -230,7 +240,7 @@ Vector.prototype.normalize = function(v) {
         this.div(m);
     }
 };
-Vector.prototype.limit = function(max) {    
+Vector.prototype.limit = function(max) {
     if (this.mag() > max) {
         this.normalize();
         this.mult(max);
@@ -247,9 +257,9 @@ function Mover(location, velocity, acceleration) {
     this.acceleration = acceleration || new Vector(0, 0);
     this.topSpeed = 5;
 }
-Mover.prototype.update = function() {    
+Mover.prototype.update = function() {
     this.velocity.add(this.acceleration);
-    this.velocity.limit(this.topSpeed);    
+    this.velocity.limit(this.topSpeed);
     this.location.add(this.velocity);
 };
 Mover.prototype.display = function(ctx, noclear) {
@@ -269,7 +279,7 @@ Mover.prototype.checkEdges = function(width, height) {
     } else if (this.location.x < 0) {
         this.location.x = width;
     }
- 
+
     if (this.location.y > height) {
         this.location.y = 0;
     } else if (this.location.y < 0) {
@@ -296,8 +306,8 @@ Mover.prototype.checkEdges = function(width, height) {
         } else {
           this.y--;
         }
-    };    
-    
+    };
+
     var w = new Walker(WIDTH/2, HEIGHT/2);
     var ew = new EgWidget('example_0_1', w.display.bind(w), w.step.bind(w), function() {
         w.x = ew.width/2;
@@ -306,12 +316,12 @@ Mover.prototype.checkEdges = function(width, height) {
     ew.run();
 })();
 
-(function example_0_2() {    
+(function example_0_2() {
     var randomCounts = [];
     randomCounts.length = 20;
-    
+
     var ew = new EgWidget('example_0_2', function(ctx) {
-        var w = ew.width / randomCounts.length;    
+        var w = ew.width / randomCounts.length;
         for (var x = 0; x < randomCounts.length; x++) {
             ctx.fillRect(x * w, ew.height - randomCounts[x], w - 1, randomCounts[x]);
         }
@@ -349,13 +359,13 @@ Mover.prototype.checkEdges = function(width, height) {
           this.y--;
         }
     };
-    
+
     var w = new Walker(WIDTH/2, HEIGHT/2);
     var ew = new EgWidget('example_0_3', w.display.bind(w), w.step.bind(w), function() {
         w.x = ew.width/2;
         w.y = ew.height/2;
     });
-    ew.run(); 
+    ew.run();
 })();
 
 (function example_0_4() {
@@ -368,7 +378,7 @@ Mover.prototype.checkEdges = function(width, height) {
         x = Random.gauss(320, 60);
     });
     ew.context.globalAlpha = 0.1;
-    ew.run();   
+    ew.run();
 })();
 
 
@@ -386,14 +396,14 @@ Mover.prototype.checkEdges = function(width, height) {
         ctx.arc(this.x, this.y, 16, 0, 2 * Math.PI, false);
         ctx.fill();
     };
-    Walker.prototype.step = function() {        
+    Walker.prototype.step = function() {
         this.x = Util.map(Random.noise(this.tx), 0, 1, 0, ew.width);
-        this.y = Util.map(Random.noise(this.ty), 0, 1, 0, ew.height);   
-   
+        this.y = Util.map(Random.noise(this.ty), 0, 1, 0, ew.height);
+
         this.tx += 0.01;
         this.ty += 0.01;
     };
-    
+
     var w = new Walker;
     var ew = new EgWidget('example_0_5', w.display.bind(w), w.step.bind(w), function() {
         w.tx = 0;
@@ -405,28 +415,28 @@ Mover.prototype.checkEdges = function(width, height) {
 (function example_0_6() {
     var ew = new EgWidget('example_0_6', function(ctx) {
         var imgData = ctx.createImageData(ew.width, ew.height);
-        var xoff = 0.0; 
-        for (var x = 0; x < imgData.width; x++) {    
-            var yoff = 0.0;     
+        var xoff = 0.0;
+        for (var x = 0; x < imgData.width; x++) {
+            var yoff = 0.0;
             for (var y = 0; y < imgData.height; y++) {
-                var bright = Util.map(Random.noise(xoff, yoff), 0, 1, 0, 255);    
+                var bright = Util.map(Random.noise(xoff, yoff), 0, 1, 0, 255);
                 imgData.data[(x + y * imgData.width) * 4] = imgData.data[(x + y * imgData.width) * 4 + 1] = imgData.data[(x + y * imgData.width) * 4 + 2] = bright;
                 imgData.data[(x + y * imgData.width) * 4 + 3] = 255;
                 yoff += 0.01;
-            }    
+            }
             xoff += 0.01;
         }
         ctx.putImageData(imgData, 0, 0);
     });
-    ew.run();    
+    ew.run();
 })();
 
-(function example_1_1() {    
+(function example_1_1() {
     var x = 100;
     var y = 100;
     var xspeed = 1;
     var yspeed = 3.3;
-    
+
     var ew = new EgWidget('example_1_1', function(ctx) {
         ctx.canvas.width = ctx.canvas.width;
         ctx.beginPath();
@@ -450,13 +460,13 @@ Mover.prototype.checkEdges = function(width, height) {
     ew.run();
 })();
 
-(function example_1_2() {    
+(function example_1_2() {
     var location = new Vector(100, 100);
     var velocity = new Vector(2.5, 5);
     var ew = new EgWidget('example_1_2', function(ctx) {
         ctx.canvas.width = ctx.canvas.width;
         ctx.beginPath();
-        ctx.arc(location.x, location.y, 16, 0, 2 * Math.PI, false);        
+        ctx.arc(location.x, location.y, 16, 0, 2 * Math.PI, false);
         ctx.fill();
     }, function() {
         location.add(velocity);
@@ -470,49 +480,49 @@ Mover.prototype.checkEdges = function(width, height) {
         location.set(100, 100);
         velocity.set(2.5, 5);
     });
-    ew.run();    
+    ew.run();
 })();
 
 
 (function example_1_3() {
     var center = new Vector(WIDTH/2, HEIGHT/2);
     var ew = new EgWidget('example_1_3', function(ctx) {
-        ctx.clearRect(-ew.width/2, -ew.height/2, ew.width, ew.height);        
-        
+        ctx.clearRect(-ew.width/2, -ew.height/2, ew.width, ew.height);
+
         var dir = Vector.sub(ew.mouse, center);
         ctx.beginPath();
         ctx.moveTo(0, 0);
         ctx.lineTo(dir.x, dir.y);
-        ctx.stroke();  
+        ctx.stroke();
     }, function() {});
     ew.context.translate(center.x, center.y);
-    ew.run();   
+    ew.run();
 })();
 
 (function example_1_4() {
     var center = new Vector(WIDTH/2, HEIGHT/2);
     var ew = new EgWidget('example_1_4', function(ctx) {
-        ctx.clearRect(-ew.width/2, -ew.height/2, ew.width, ew.height);        
-        
+        ctx.clearRect(-ew.width/2, -ew.height/2, ew.width, ew.height);
+
         var dir = Vector.sub(ew.mouse, center);
         dir.mult(0.5);
         ctx.beginPath();
         ctx.moveTo(0, 0);
         ctx.lineTo(dir.x, dir.y);
-        ctx.stroke();  
+        ctx.stroke();
     }, function() {});
     ew.context.translate(center.x, center.y);
-    ew.run();   
+    ew.run();
 })();
 
 (function example_1_5() {
     var center = new Vector(WIDTH/2, HEIGHT/2);
     var ew = new EgWidget('example_1_5', function(ctx) {
-        ctx.clearRect(0, 0, ew.width, ew.height);        
-        
-        var dir = Vector.sub(ew.mouse, center);       
+        ctx.clearRect(0, 0, ew.width, ew.height);
+
+        var dir = Vector.sub(ew.mouse, center);
         ctx.fillRect(0, 0, dir.mag(), 10);
-        
+
         ctx.save();
         ctx.translate(center.x, center.y);
         ctx.beginPath();
@@ -520,29 +530,29 @@ Mover.prototype.checkEdges = function(width, height) {
         ctx.lineTo(dir.x, dir.y);
         ctx.stroke();
         ctx.restore();
-    }, function() {});    
-    ew.run();   
+    }, function() {});
+    ew.run();
 })();
 
 (function example_1_6() {
     var center = new Vector(WIDTH/2, HEIGHT/2);
     var ew = new EgWidget('example_1_6', function(ctx) {
-        ctx.clearRect(-ew.width/2, -ew.height/2, ew.width, ew.height);        
-        
+        ctx.clearRect(-ew.width/2, -ew.height/2, ew.width, ew.height);
+
         var dir = Vector.sub(ew.mouse, center);
         dir.normalize();
         dir.mult(50);
         ctx.beginPath();
         ctx.moveTo(0, 0);
         ctx.lineTo(dir.x, dir.y);
-        ctx.stroke();  
+        ctx.stroke();
     }, function() {});
     ew.context.translate(center.x, center.y);
-    ew.run();   
+    ew.run();
 })();
 
 (function example_1_7() {
-    var m = new Mover(new Vector(Random.arbitrary(0, WIDTH), Random.arbitrary(0, HEIGHT)), 
+    var m = new Mover(new Vector(Random.arbitrary(0, WIDTH), Random.arbitrary(0, HEIGHT)),
                       new Vector(Random.arbitrary(-2, 2), Random.arbitrary(-2, 2)));
     var ew = new EgWidget('example_1_7', m.display.bind(m), function() {
         m.update();
@@ -551,7 +561,7 @@ Mover.prototype.checkEdges = function(width, height) {
         m.location.set(Random.arbitrary(0, WIDTH), Random.arbitrary(0, HEIGHT));
         m.velocity.set(Random.arbitrary(-2, 2), Random.arbitrary(-2, 2));
     });
-    ew.run();   
+    ew.run();
 })();
 
 (function example_1_8() {
@@ -559,20 +569,20 @@ Mover.prototype.checkEdges = function(width, height) {
     m.topSpeed = 10;
     var ew = new EgWidget('example_1_8', m.display.bind(m), function() {
         m.update();
-        m.checkEdges(ew.width, ew.height);        
+        m.checkEdges(ew.width, ew.height);
     }, function() {
         m.location.set(WIDTH/2, HEIGHT/2);
         m.velocity.set(0, 0);
         m.acceleration.set(-0.001,0.01);
     });
-    ew.run();   
+    ew.run();
 })();
 
 (function example_1_9() {
     var m = new Mover(new Vector(WIDTH/2, HEIGHT/2));
     var ew = new EgWidget('example_1_9', m.display.bind(m), function() {
         m.acceleration = Vector.random2D();
-        m.acceleration.mult(Random.arbitrary(0, 2));        
+        m.acceleration.mult(Random.arbitrary(0, 2));
         m.update();
         m.checkEdges(ew.width, ew.height);
     }, function() {
@@ -606,7 +616,7 @@ Mover.prototype.checkEdges = function(width, height) {
     }
     var ew = new EgWidget('example_1_11', function(ctx) {
         ew.clear();
-        movers.forEach(function(m) {            
+        movers.forEach(function(m) {
             m.display(ctx, true);
         });
     }, function() {
@@ -616,14 +626,14 @@ Mover.prototype.checkEdges = function(width, height) {
             dir.mult(0.2);
             m.acceleration = dir;
             m.update();
-        });        
+        });
     }, function() {
         movers.forEach(function(m) {
             m.location.set(Random.arbitrary(0, WIDTH), Random.arbitrary(0, HEIGHT));
             m.velocity.set(0, 0);
             m.acceleration.set(0, 0);
-        });        
-    });    
+        });
+    });
     ew.run();
 })();
 
