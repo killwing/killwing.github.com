1cdcb5bdbd6b817306d2503587b22328b3350e8f
kelvin <kowochen@gmail.com>
2014-04-15 00:56:03 +0800

    update heap sort

--- js/algorithm.js
+++ js/algorithm.js
@@ -3,7 +3,7 @@
 const WIDTH = 640;
 const HEIGHT = 360;
 const N = 20;
-const SPEED = 500;
+const SPEED = 1000;
 const OFFSET = 30;
 
 // http://clrs.cc/
@@ -12,10 +12,11 @@ const COLOR = {
     SORTED: '#2ECC40',
     INACTIVE: '#DDDDDD',
     LEFT: '#01FF70',
-    RIGHT: '#3D9970'
+    RIGHT: '#3D9970',
+    HALF: '#7FDBFF'
 };
 
-function VisData() {
+function BarData() {
     this.bars = [];
     this.values = [];
     this.length = N;
@@ -32,7 +33,7 @@ function VisData() {
         this.values.push(b);
     }
 }
-VisData.prototype.swap = function(i, j) {
+BarData.prototype.swap = function(i, j) {
     if (i == j) {
         return;
     }
@@ -47,15 +48,15 @@ VisData.prototype.swap = function(i, j) {
     this.values[i] = this.values[j];
     this.values[j] = temp;
 };
-VisData.prototype.get = function(i) {
+BarData.prototype.get = function(i) {
     return this.values[i];
 };
-VisData.prototype.up = function(i) {
+BarData.prototype.up = function(i) {
     this.values[i].preX = undefined;
     this.values[i].y -= OFFSET;
     return this.values[i];
 };
-VisData.prototype.down = function(i, v) {
+BarData.prototype.down = function(i, v) {
     if (this.values[i].preX !== undefined) {
         v.preX = v.x; // backup
         v.x = this.values[i].preX;
@@ -66,16 +67,16 @@ VisData.prototype.down = function(i, v) {
     this.values[i] = v;
     this.values[i].y += OFFSET;
 }
-VisData.prototype.less = function(i, j) {
+BarData.prototype.less = function(i, j) {
     return this.values[i].h < this.values[j].h;
 };
-VisData.prototype.greaterValue = function(i, v) {
+BarData.prototype.greaterValue = function(i, v) {
     return this.values[i].h > v.h;
 };
-VisData.lessEqual = function(x, y) {
+BarData.lessEqual = function(x, y) {
     return x.h <= y.h;
 };
-VisData.prototype.assign = function(i, j) {
+BarData.prototype.assign = function(i, j) {
     if (this.emptyX !== undefined) {
         let newEmptyX = this.values[j].x;
         this.values[j].x = this.emptyX;
@@ -87,14 +88,14 @@ VisData.prototype.assign = function(i, j) {
         this.values[i] = this.values[j];
     }
 };
-VisData.prototype.assignValue = function(i, v) {
+BarData.prototype.assignValue = function(i, v) {
     if (this.emptyX !== undefined) {
         v.x = this.emptyX;
         this.values[i] = v;
         this.emptyX = undefined;
     } // else no empty pos, other value can not be assigned
 };
-VisData.prototype.setState = function(e) {
+BarData.prototype.setState = function(e) {
     if (e.s == 'GROUP') {
         this.values.forEach(function(value, i) {
             value.a = e.a(i);
@@ -111,7 +112,7 @@ VisData.prototype.setState = function(e) {
     }
 };
 
-function VisWidget(sort) {
+function BarWidget(sort) {
     this.svg = d3.select('#'+sort.name).attr('width', WIDTH).attr('height', HEIGHT);
     this.speed = SPEED;
     this.sort = sort;
@@ -146,11 +147,11 @@ function VisWidget(sort) {
     playBtn.onclick = this.onPlay.bind(this);
     svgElem.parentNode.insertBefore(playBtn, svgElem.nextSibling);
 }
-VisWidget.prototype.init = function() {
-    this.data = new VisData;
+BarWidget.prototype.init = function() {
+    this.data = new BarData;
     this.gen = new this.sort(this.data);
 };
-VisWidget.prototype.update = function() {
+BarWidget.prototype.update = function() {
     let ret = this.gen.next();
     if (ret.done) {
         this.done();
@@ -164,19 +165,19 @@ VisWidget.prototype.update = function() {
     this.svg.selectAll('rect')
         .data(this.data.bars)
         .transition()
-        .duration(SPEED)
+        .duration(this.speed)
         .attr('x', function(d, i) { return d.x; })
         .attr('y', function(d, i) { return d.y; })
         .attr('width', function(d, i) { return d.w; })
         .attr('height', function(d, i) { return d.h; })
         .attr('fill', function(d, i) { return d.a ? COLOR[d.s] : COLOR.INACTIVE; })
 };
-VisWidget.prototype.onPlay = function() {
+BarWidget.prototype.onPlay = function() {
     if (!this.timerId) {
         this.timerId = setInterval(this.update.bind(this), this.speed);
     }
 };
-VisWidget.prototype.onStep = function() {
+BarWidget.prototype.onStep = function() {
     // stop play first
     if (this.timerId) {
         clearInterval(this.timerId);
@@ -185,7 +186,7 @@ VisWidget.prototype.onStep = function() {
         this.update();
     }
 };
-VisWidget.prototype.onReset = function() {
+BarWidget.prototype.onReset = function() {
     clearInterval(this.timerId);
     this.timerId = null;
     this.init();
@@ -198,10 +199,200 @@ VisWidget.prototype.onReset = function() {
         .attr('height', function(d, i) { return d.h; })
         .attr('fill', COLOR.UNSORTED);
 };
-VisWidget.prototype.done = function() {
+BarWidget.prototype.done = function() {
     clearInterval(this.timerId);
     this.timerId = null;
-    this.svg.selectAll('rect').attr('fill', COLOR.SORTED);
+    this.svg.selectAll('rect').transition().attr('fill', COLOR.SORTED);
+};
+
+
+function TreeData() {
+    this.length = N;
+    this.values = [];
+    for (let i = 0; i < this.length; ++i) {
+        this.values.push({ value: Math.floor(Math.random() * 100), s: 'UNSORTED', children: [] });
+    }
+
+    // build tree from array
+    let tree = {};
+    let i = 0;
+    let q = [];
+    if (i < this.length) {
+        let node = this.values[i++];
+        tree = node;
+        q.push(node);
+    }
+    while (i < this.length) {
+        let p = q.shift();
+
+        let nodeL = this.values[i++];
+        p.children.push(nodeL);
+        q.push(nodeL);
+
+        if (i < this.length) {
+            let nodeR = this.values[i++];
+            p.children.push(nodeR);
+            q.push(nodeR);
+        }
+    }
+
+    this.tree = d3.layout.tree().size([WIDTH, HEIGHT]);
+    this.nodes = this.tree.nodes(tree);
+    this.links = this.tree.links(this.nodes);
+
+    // set height between nodes
+    this.nodes.forEach(function(d) { d.y = d.depth * 70 + 30; });
+}
+TreeData.prototype.swap = function(i, j) {
+    if (i == j) {
+        return;
+    }
+
+    // swap pos first
+    let temp = this.values[i].x;
+    this.values[i].x = this.values[j].x;
+    this.values[j].x = temp;
+
+    temp = this.values[i].y;
+    this.values[i].y = this.values[j].y;
+    this.values[j].y = temp;
+
+    //// swap the node referenced
+    temp = this.values[i];
+    this.values[i] = this.values[j];
+    this.values[j] = temp;
+};
+TreeData.prototype.greater = function(i, j) {
+    return this.values[i].value > this.values[j].value;
+};
+TreeData.prototype.greaterEqual = function(i, j) {
+    return this.values[i].value >= this.values[j].value;
+};
+TreeData.prototype.setState = function(e) {
+    if (e.i instanceof Array) { // set range
+        for (var i = e.i[0]; i <= e.i[1]; ++i) {
+            this.values[i].s = e.s;
+        };
+    } else {
+        this.values[e.i].s = e.s;
+        if (e.ii !== undefined) { // extra
+            this.values[e.ii].s = e.ss;
+        }
+    }
+};
+
+function TreeWidget(sort) {
+    this.svg = d3.select('#'+sort.name).attr('width', WIDTH).attr('height', HEIGHT);
+    this.speed = SPEED;
+    this.sort = sort;
+
+    this.diagonal = d3.svg.diagonal().projection(function(d) { return [d.x, d.y]; });
+
+    this.init();
+
+    // init draw
+    this.svg.selectAll('path')
+        .data(this.data.links)
+        .enter()
+        .append('path')
+        .attr('d', this.diagonal)
+        .attr('stroke', COLOR.INACTIVE)
+        .attr('stroke-width', '2px')
+        .attr('fill', 'none');
+
+    let svgNodes = this.svg.selectAll('g')
+        .data(this.data.nodes)
+        .enter()
+        .append('g')
+        .attr('transform', function(d) { return 'translate(' + d.x + ',' + d.y + ')'; });
+
+    svgNodes.append('circle')
+        .attr('r', 20)
+        .attr('fill', function(d) { return COLOR[d.s]; });
+
+    svgNodes.append('text')
+        .attr('text-anchor', 'middle')
+        .text(function(d) { return d.value; });
+
+    // controls
+    let svgElem = document.getElementById(sort.name);
+    let resetBtn = document.createElement('button');
+    resetBtn.innerHTML = 'RESET';
+    resetBtn.onclick = this.onReset.bind(this);
+    svgElem.parentNode.insertBefore(resetBtn, svgElem.nextSibling);
+
+    let stepBtn = document.createElement('button');
+    stepBtn.innerHTML = 'STEP';
+    stepBtn.onclick = this.onStep.bind(this);
+    svgElem.parentNode.insertBefore(stepBtn, svgElem.nextSibling);
+
+    let playBtn = document.createElement('button');
+    playBtn.innerHTML = 'PLAY';
+    playBtn.onclick = this.onPlay.bind(this);
+    svgElem.parentNode.insertBefore(playBtn, svgElem.nextSibling);
+}
+TreeWidget.prototype.init = function() {
+    this.data = new TreeData;
+    this.gen = new this.sort(this.data);
+};
+TreeWidget.prototype.update = function() {
+    let ret = this.gen.next();
+    if (ret.done) {
+        this.done();
+        return;
+    }
+
+    // update state
+    this.data.setState(ret.value);
+
+    // redraw
+    this.svg.selectAll('g')
+        .data(this.data.nodes)
+        .transition()
+        .duration(this.speed)
+        .attr('transform', function(d) { return 'translate(' + d.x + ',' + d.y + ')'; });
+
+    this.svg.selectAll('g circle')
+        .data(this.data.nodes)
+        .transition()
+        .duration(this.speed)
+        .attr('fill', function(d) { return COLOR[d.s]; });
+};
+TreeWidget.prototype.onPlay = function() {
+    if (!this.timerId) {
+        this.timerId = setInterval(this.update.bind(this), this.speed);
+    }
+};
+TreeWidget.prototype.onStep = function() {
+    // stop play first
+    if (this.timerId) {
+        clearInterval(this.timerId);
+        this.timerId = null;
+    } else {
+        this.update();
+    }
+};
+TreeWidget.prototype.onReset = function() {
+    clearInterval(this.timerId);
+    this.timerId = null;
+    this.init();
+
+    this.svg.selectAll('g')
+        .data(this.data.nodes)
+        .attr('transform', function(d) { return 'translate(' + d.x + ',' + d.y + ')'; });
+
+    this.svg.selectAll('g circle')
+        .data(this.data.nodes)
+        .attr('fill', function(d) { return COLOR.UNSORTED; });
+
+    this.svg.selectAll('g text')
+        .data(this.data.nodes)
+        .text(function(d) { return d.value; });
+};
+TreeWidget.prototype.done = function() {
+    clearInterval(this.timerId);
+    this.timerId = null;
+    this.svg.selectAll('g circle').transition().attr('fill', COLOR.SORTED);
 };
 
 function *selection(a) {
@@ -216,7 +407,7 @@ function *selection(a) {
         yield {i: i, s: 'SORTED'};
     }
 }
-new VisWidget(selection);
+new BarWidget(selection);
 
 function *insertion(a) {
     yield {i: 0, s: 'SORTED'}; // the 1st one is always sorted, change state for it
@@ -234,7 +425,7 @@ function *insertion(a) {
         yield {i: j, s: 'SORTED'};
     }
 }
-new VisWidget(insertion);
+new BarWidget(insertion);
 
 function *shell(a) {
     let d = 1;
@@ -276,7 +467,7 @@ function *shell(a) {
         d = Math.floor(d/3);
     }
 };
-new VisWidget(shell);
+new BarWidget(shell);
 
 function *quick(a, l, r) {
     l = (l == undefined) ? 0 : l;
@@ -309,7 +500,7 @@ function *quick(a, l, r) {
         yield {i: r, s: 'SORTED'};
     }
 };
-new VisWidget(quick);
+new BarWidget(quick);
 
 
 function *merge(a, l, r) {
@@ -344,7 +535,7 @@ function *merge(a, l, r) {
             a.down(i, aux[y++]);
         } else if (y > r) {
             a.down(i, aux[x++]);
-        } else if (VisData.lessEqual(aux[x], aux[y])) {
+        } else if (BarData.lessEqual(aux[x], aux[y])) {
             a.down(i, aux[x++]);
         } else {
             a.down(i, aux[y++]);
@@ -352,5 +543,43 @@ function *merge(a, l, r) {
         yield {i: i, s: 'SORTED'};
     }
 }
-new VisWidget(merge);
+new BarWidget(merge);
 
+function *heap(a) {
+    function *sink(l, r) {
+        let j = l;
+        while (2*j+1 <= r) {
+            let p = 2*j+1;
+            if (p+1 <= r && a.greater(p+1, p)) {
+                ++p;
+            }
+            if (a.greaterEqual(j, p)) {
+                yield {i: j, s: 'HALF'};
+                break;
+            }
+            a.swap(p, j);
+            j = p;
+
+            // update state
+            if (2*p+1 <= r) {
+                yield {i: p, s: 'UNSORTED'};
+            } else {
+                yield {i: p, s: 'HALF'};
+            }
+        }
+    };
+
+    let n = a.length - 1;
+    yield {i: [Math.floor((n-1)/2)+1, n], s: 'HALF'};
+    for (let i = Math.floor((n-1)/2); i >= 0; --i) {
+        yield *sink(i, n);
+    }
+
+    while (n > 0) {
+        a.swap(0, n);
+        yield {i: n, s: 'SORTED', ii: 0, ss: 'UNSORTED'};
+        n--;
+        yield *sink(0, n);
+    }
+}
+new TreeWidget(heap);
